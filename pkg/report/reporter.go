30576000726205997609f140bdc63456c12d12de
M-Faheem-Khan <faheem5948@gmail.com>
2024-04-09 00:17:10 -0400

    add report
    Signed-off-by: M-Faheem-Khan <faheem5948@gmail.com>

--- pkg/report/reporter.go
+++ pkg/report/reporter.go
@@ -1,25 +1,55 @@
 package report
 
-import "github.com/m-faheem-khan/Doc-Analysis-Helper/pkg/iocs"
+import (
+	"fmt"
+	"os"
+
+	"github.com/m-faheem-khan/Doc-Analysis-Helper/pkg/iocs"
+)
 
 type IOCS struct {
 	OutBoundConnections []iocs.OutBoundConnection
 }
 
-func GenerateReport() {
+type REPORT struct {
+	FileName               string
+	SHA256Hash             []byte
+	AnalysisDate           string
+	IndicatorsOfCompromise IOCS
+}
 
-	// File: xxx
-	// SHA256 Hash: xxx
-	// Date Analyze: xxx
+func (r *REPORT) PrintReport() {
+	fmt.Printf("File: %s\n", r.FileName)
+	fmt.Printf("SHA256 Hash: %x\n", r.SHA256Hash)
+	fmt.Printf("Date Analyzed: %s\n", r.AnalysisDate)
 
-	// ----- IOCs -----
-	// ## OutBoundConnections
-	// Potential x outbound connections were found.
-	// PATH
-	// IOC
-	// ...
-	// PATH
-	// IOC
-	// ----- IOCs -----
+	fmt.Printf("\n\n\n")
+	for _, obc := range r.IndicatorsOfCompromise.OutBoundConnections {
+		fmt.Printf("IOC: %s\n", obc.IOC)
+		fmt.Printf("File Path: %s\n", obc.IOC)
+	}
+}
 
+func (r *REPORT) WriteReport(fname string) {
+	f, err := os.Create(fname)
+	if err != nil {
+		panic(err)
+	}
+
+	f.Write([]byte("File: " + r.FileName + "\n"))
+	f.Write([]byte("SHA256 Hash: "))
+	f.Write(r.SHA256Hash)
+	f.Write([]byte("\n"))
+	f.Write([]byte("Date Analyzed: " + r.AnalysisDate + "\n"))
+
+	f.Write([]byte("---------------\n\n\n"))
+
+	f.Write([]byte("## Indicators of Compromise \n\n"))
+	f.Write([]byte("### Out Bound Connections \n\n"))
+	for _, obc := range r.IndicatorsOfCompromise.OutBoundConnections {
+		f.Write([]byte("**IOC:** " + obc.IOC + "\n"))
+		f.Write([]byte("**File Path:** `" + obc.Path + "`\n"))
+	}
+
+	f.Write([]byte("\n"))
 }
